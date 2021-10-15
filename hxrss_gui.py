854de5545fee0789a667aa762f6ad3da431fd117
Christoph Lechner <christoph.lechner@desy.de>
2021-10-15 18:17:10 +0200

    I/O: more obj + code moved to additional file

--- hxrss_gui.py
+++ hxrss_gui.py
@@ -8,45 +8,12 @@ from PyQt5.QtCore import QTimer
 import threading,queue
 import time
 from types import SimpleNamespace
-import enum
 from copy import deepcopy
-import pydoocs
+
+from hxrss_io import thread_ioworker, IO_Cmd, get_initial_photon_energy_value
 
 # from clscratch import stuff2000
 import do_crystal_plot
-
-class IO_Cmd(enum.Enum):
-    IO_JUSTREAD = enum.auto()
-    IO_QUIT = enum.auto()
-    IO_SET = enum.auto()
-
-
-def thread_ioworker(qin, qout):
-    message_counter=0
-    while True:
-        item = qin.get()
-        print('io thread: got task from queue')
-        if item.cmd==IO_Cmd.IO_QUIT:
-            print('io thread: got QUIT cmd ==> leaving')
-            break
-        tstart = time.time()
-        # do work
-        if item.cmd==IO_Cmd.IO_SET:
-            print('io thread: got set command (currently not implemented)')
-
-        r = SimpleNamespace()
-        r.tag = message_counter
-        r.timestamp = time.time() # timestamp to recognise 'old' msgs
-        r.color1_rb = 123
-        r.color2_rb = 234
-        r.color3_rb = 456
-        x = pydoocs.read('XFEL.FEL/UNDULATOR.SASE2/MONOPA.2307.SA2/MOTEMP')
-        r.mono2_pitch = x['data']
-        tend = time.time()
-        print('io thread: finishing item, processing time: {}'.format(tend-tstart))
-        qin.task_done()
-        qout.put(r)
-        message_counter+=1
 
 class MainWindow(qtw.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
@@ -57,15 +24,32 @@ class MainWindow(qtw.QMainWindow, Ui_MainWindow):
         self.display_map_button.clicked.connect(self.on_show_map_button)
         self.apply_button.clicked.connect(self.on_apply_button)
 
+        self.photon_energy_edit.setText('{:.2f}'.format(get_initial_photon_energy_value()))
+
+        # thread for communication with machine: display dbg messages?
+        self.io_thread_dbg=False
         # queue for communication to machine
         self.q_to_io = queue.Queue(maxsize=1000)
         self.q_from_io = queue.Queue(maxsize=1000)
-        # start machine communication thread
-        threading.Thread(target=thread_ioworker, args=(self.q_to_io,self.q_from_io), daemon=True).start()
+        # start machine communication thread (specify daemon=True when the thread should automatically be terminated)
+        # threading.Thread(target=thread_ioworker, args=(self.q_to_io,self.q_from_io), daemon=True).start()
+        threading.Thread(target=thread_ioworker, args=(self.q_to_io,self.q_from_io,self.io_thread_dbg)).start()
         # setup Qt timer for machine IO (for updating current status)
         self.update_timer = QTimer()
         self.update_timer.timeout.connect(self.timeout)
         self.update_timer.start(1000)
+
+
+    def closeEvent(self, event):
+        print('got close event, stopping IO thread')
+
+        # send to IO thread
+        cmd = SimpleNamespace()
+        cmd.cmd = IO_Cmd.IO_QUIT
+        self.q_to_io.put(cmd)
+
+        event.accept()
+        # to ignore this event: event.ignore()
 
 
     # Data was received from IO thread
@@ -74,13 +58,22 @@ class MainWindow(qtw.QMainWindow, Ui_MainWindow):
         self.color1_rb_display.setText(str(msg.color1_rb))
         self.color2_rb_display.setText(str(msg.color2_rb))
         self.color3_rb_display.setText(str(msg.color3_rb))
-        self.color1_rb_display.setText(str(msg.tag))
-        self.mono2_pitch_display.setText(str(msg.mono2_pitch))
+        self.mono2_pitch_rb_display.setText(str(msg.mono2_pitch_rb))
+        self.mono2_pitch_sp_display.setText(str(msg.mono2_pitch_sp))
+        self.mono2_roll_rb_display.setText(str(msg.mono2_roll_rb))
+        self.mono2_roll_sp_display.setText(str(msg.mono2_roll_sp))
+        self.io_msgtag_display.setText(str(msg.tag))
+        self.io_processingtime_display.setText('{:.2f}'.format(1e3*msg.processing_time))
+        self.io_msgage_display.setText('{:.2f}'.format(msg.age))
 
     def timeout(self):
+        self.io_thread_dbg = self.io_threaddbg_checkbox.checkState()
+        dbg = self.io_thread_dbg
+
         # send to IO thread
         cmd = SimpleNamespace()
         cmd.cmd = IO_Cmd.IO_JUSTREAD
+        cmd.io_dbg = dbg
         self.q_to_io.put(cmd)
 
         got_something=False
@@ -90,13 +83,13 @@ class MainWindow(qtw.QMainWindow, Ui_MainWindow):
             while True:
                 msg = deepcopy( self.q_from_io.get(block=False) )
                 self.q_from_io.task_done() # to be sure, data was copied (TODO: determine if needed)
-                msg_age = time.time()-msg.timestamp
+                msg.age = time.time()-msg.timestamp
                 got_something=True
-                print('got event ID={}, age={}s'.format(msg.tag, msg_age))
+                if dbg: print('GUI timer: got event ID={}, age={}s'.format(msg.tag, msg.age))
                 self.update_readbacks(msg)
         except queue.Empty:
             if got_something==False:
-                print('there was nothing in queue')
+                if dbg: print('GUI timer: there was nothing in queue')
 
     def on_show_map_button(self):
         x = self.photon_energy_edit.text()
---
+++ hxrss_io.py
@@ -0,0 +1,69 @@
+import threading,queue
+import time
+from types import SimpleNamespace
+import enum
+from copy import deepcopy
+import pydoocs
+
+class IO_Cmd(enum.Enum):
+    IO_JUSTREAD = enum.auto()
+    IO_QUIT = enum.auto()
+    IO_SET = enum.auto()
+
+def simple_doocs_read(addr):
+    x = pydoocs.read(addr)
+    v = x['data']
+    return v
+    
+
+def thread_ioworker(qin, qout, dbg=False):
+    processing_time_warn=1 # seconds
+    message_counter=0
+    while True:
+        item = qin.get()
+        if hasattr(item, 'io_dbg'):
+            dbg=item.io_dbg
+        if dbg: print('io thread: got task from queue')
+        if item.cmd==IO_Cmd.IO_QUIT:
+            print('io thread: got QUIT cmd ==> leaving')
+            break
+        tstart = time.time()
+        # do work
+        if item.cmd==IO_Cmd.IO_SET:
+            print('io thread: got set command (currently not implemented)')
+
+        r = SimpleNamespace()
+        r.tag = message_counter
+        r.timestamp = time.time() # timestamp to recognise 'old' msgs
+        r.color1_rb = simple_doocs_read('XFEL.FEL/WAVELENGTHCONTROL.SA2/XFEL.SA2.COLOR1/E_PHOTON')
+        r.color2_rb = 234
+        r.color3_rb = 456
+        r.mono2_pitch_rb = simple_doocs_read('XFEL.FEL/UNDULATOR.SASE2/MONOPA.2307.SA2/ANGLE')
+        r.mono2_pitch_sp = simple_doocs_read('XFEL.FEL/UNDULATOR.SASE2/MONOPA.2307.SA2/ANGLE.SET')
+        r.mono2_roll_rb  = simple_doocs_read('XFEL.FEL/UNDULATOR.SASE2/MONORA.2307.SA2/ANGLE')
+        r.mono2_roll_sp  = simple_doocs_read('XFEL.FEL/UNDULATOR.SASE2/MONORA.2307.SA2/ANGLE.SET')
+        tend = time.time()
+        dt = tend-tstart
+        r.processing_time = dt
+        if dbg:
+            print('io thread: finishing item, processing time: {}'.format(dt))
+        else:
+            if dt>processing_time_warn:
+                print('io thread: finishing item, excessive processing time was: {}'.format(dt))
+        qin.task_done()
+        qout.put(r)
+        message_counter+=1
+    print('io thread is finishing')
+
+
+#####
+
+# Initial photon energy value to be displayed on the GUI
+# Value is fetched once during start-up of the GUI
+def get_initial_photon_energy_value():
+    print('Reading SASE2 undulator color1 set point as initial value for photon energy field')
+    x = pydoocs.read('XFEL.FEL/WAVELENGTHCONTROL.SA2/XFEL.SA2.COLOR1/E_PHOTON')
+    value = x['data']
+    return value
+    
+
