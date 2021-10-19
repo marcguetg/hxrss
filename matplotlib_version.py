39c35e85e40db9aac4ebb26fc3361a310699ee3d
Christoph Lechner <christoph.lechner@desy.de>
2021-10-19 03:56:55 +0200

    misc files

---
+++ matplotlib_version.py
@@ -0,0 +1,7 @@
+import matplotlib
+import numpy
+import sys
+
+print('Python version '+str(sys.version))
+print('matplotlib '+str(matplotlib.__version__))
+print('numpy '+str(numpy.version.version))
